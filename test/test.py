a504c0cedc1da16654111f0b93b6c2b9a26d5939
Alex Petricenco <apetricenco@wyze.com>
2022-01-14 18:27:28 -0800

    creating first dict of first player

--- test/test.py
+++ test/test.py
@@ -1,17 +1,45 @@
 import requests
 import json
+from requests.auth import HTTPBasicAuth
+from bs4 import BeautifulSoup
+# from selenium import webdriver
+# from selenium.webdriver.common.keys import Keys
+# import time
+# import webbrowser, os
+# from PIL import Image
 
-URL = 'https://api.publicapis.org/entries'
+URL = 'https://www.premierleague.com/players'
 
-r = requests.get(URL)
-# print(r.text)
-res = json.dumps(r.text)
+page = requests.get(URL, auth=HTTPBasicAuth('warllokc@yahoo.com', 'Petrici-89'))
+soup = BeautifulSoup(page.content, "html.parser")
+# results = soup.find_all("tbody", class_="dataContainer indexSection")
 
-print(len(res))
+# test from book
+players_table =soup.select('.playerName')
+# print((type(players_table)))
+# print(len(players_table))
+# print(type(players_table[0]))
+# print(str(players_table[0]))
+# res = players_table[0].getText()
+# print(res)
+dictionary_with_elements_attr = players_table[0].attrs
+# print(dictionary_with_elements_attr.get('href'))
+empty_player_dictionary = {}
+for players in players_table:
+    empty_player_dictionary[players_table[0].getText()] = dictionary_with_elements_attr.get('href')
+
+print(empty_player_dictionary)
+
+
+
+
+
+# res = json.dumps(page.text)
+#
+# print(len(res))
 
 # requests.put()
 
-code = r.status_code
+code = page.status_code
 
 print(code)
-# print(r.status_code)
