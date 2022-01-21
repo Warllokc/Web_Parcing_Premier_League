ddd4b897e8ccb08d2eeeb0f892630f25862f61aa
Alex Petricenco <apetricenco@wyze.com>
2022-01-21 07:43:46 -0800

    parse through all players

--- test/main.py
+++ test/main.py
@@ -20,12 +20,12 @@ empty_player_dictionary = {}
 def scrape_player_data():
     id = 1
     while True:
+        try:
+            empty_player_dictionary["id"] = id
 
-        empty_player_dictionary["id"] = id
+            URL = "https://www.premierleague.com/players/" + str(id)
 
-        URL = "https://www.premierleague.com/players/" + str(id)
-
-        headers = {"accept": "*/*",
+            headers = {"accept": "*/*",
                "accept-language": "en-US,en;q=0.9",
                "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
                "origin": "https://www.premierleague.com",
@@ -38,51 +38,43 @@ def scrape_player_data():
                "sec-fetch-site": "cross-site",
                "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36}"}
 
-        rec = requests.get(url=URL, headers=headers)
-        if rec.status_code == 200:
-            soup = BeautifulSoup(rec.content, "html.parser")
-            player_data = soup.findAll("div", class_ ="info")
-            player_country = soup.findAll("span", class_ ="playerCountry")
+            rec = requests.get(url=URL, headers=headers)
+            if rec.status_code == 200:
+                soup = BeautifulSoup(rec.content, "html.parser")
+                player_data = soup.findAll("div", class_ ="info")
+                player_country = soup.findAll("span", class_ ="playerCountry")
+                picture = soup.findAll('img')[42]
 
-            ########
-            # if not player_data[0].next:
-            #     id+=1
-            # #######
+                # Adding to dictionary name, country of origin
+                empty_player_dictionary["name"] = picture.get('alt')
+                empty_player_dictionary["country"] = player_country[0].next
 
-            picture = soup.findAll('img')[42]
+                if "Photo-Missing" in picture.get('src') and picture.get('data-player'):
+                    a_string = str(picture.get('src')).replace("Photo-Missing", str(picture.get('data-player')))
 
-            # Adding to dictionary name, country of origin
-            empty_player_dictionary["name"] = picture.get('alt')
-            empty_player_dictionary["country"] = player_country[0].next
+                # Adding to dictionary image_link
+                    empty_player_dictionary["image_link"] = a_string.replace("//", "")
+                else:
 
-            if "Photo-Missing" in picture.get('src') and picture.get('data-player'):
-                a_string = str(picture.get('src')).replace("Photo-Missing", str(picture.get('data-player')))
+                # Adding to dictionary image_link
+                    empty_player_dictionary["image_link"] = picture.get('src').replace("//", "")
 
-            # Adding to dictionary image_link
-                empty_player_dictionary["image_link"] = a_string.replace("//", "")
+                # Adding to dictionary position, date of birth, height and link
+                empty_player_dictionary["position"] = player_data[0].next
+                empty_player_dictionary["date_of_birth"] = player_data[2].next.strip()
+                empty_player_dictionary["height"] = player_data[3].next
+                empty_player_dictionary["player_link"] = URL
+
+                id += 1
+
+                print(empty_player_dictionary)
+
             else:
-
-            # Adding to dictionary image_link
-                empty_player_dictionary["image_link"] = picture.get('src').replace("//", "")
-
-            # Adding to dictionary position, date of birth, height and link
-            empty_player_dictionary["position"] = player_data[0].next
-            empty_player_dictionary["date_of_birth"] = player_data[2].next.strip()
-            empty_player_dictionary["height"] = player_data[3].next
-            empty_player_dictionary["player_link"] = URL
-
-            id += 1
-
-
-            print(empty_player_dictionary)
-
-        else:
-            print("end of Parsing")
-            break
-        if id == 30 or id == 120:
-            id += 2
-        elif id == 63 or id ==90 or id == 144:
+                print("end of Parsing")
+                break
+        except IndexError:
             id+=1
+            continue
 
 scrape_player_data()
 
