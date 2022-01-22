7b4c9d9089865fbbec23cd508b77d5af94b77f1a
Alex Petricenco <apetricenco@wyze.com>
2022-01-21 17:10:21 -0800

    add main file

--- test/main.py
+++ test/main.py
@@ -1,35 +1,12 @@
-from collections import defaultdict
+import requests
+import json
 
-import requests
-import json
-from requests.auth import HTTPBasicAuth
 from bs4 import BeautifulSoup
-import time
-import requests
-import json
-from requests.auth import HTTPBasicAuth
-from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException, \
-    ElementNotInteractableException
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.service import Service
 
 empty_player_dictionary = {}
 
-
-def scrape_player_data():
-    id = 1
-
-    while True:
-        try:
-
-            empty_player_dictionary["id"] = id
-
-            URL = "https://www.premierleague.com/players/" + str(id)
-
-            headers = {"accept": "*/*",
+URL = "https://www.premierleague.com/players/"
+headers = {"accept": "*/*",
                "accept-language": "en-US,en;q=0.9",
                "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
                "origin": "https://www.premierleague.com",
@@ -42,50 +19,56 @@ def scrape_player_data():
                "sec-fetch-site": "cross-site",
                "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36}"}
 
-            rec = requests.get(url=URL, headers=headers)
-            if rec.status_code == 200:
-                soup = BeautifulSoup(rec.content, "html.parser")
-                player_data = soup.findAll("div", class_ ="info")
-                player_country = soup.findAll("span", class_ ="playerCountry")
-                picture = soup.findAll('img')[42]
-
-                # Adding to dictionary name, country of origin
-                empty_player_dictionary["name"] = picture.get('alt')
-                empty_player_dictionary["country"] = player_country[0].next
-
-                if "Photo-Missing" in picture.get('src') and picture.get('data-player'):
-                    a_string = str(picture.get('src')).replace("Photo-Missing", str(picture.get('data-player')))
-
-                # Adding to dictionary image_link
-                    empty_player_dictionary["image_link"] = a_string.replace("//", "")
-                else:
-
-                # Adding to dictionary image_link
-                    empty_player_dictionary["image_link"] = picture.get('src').replace("//", "")
-
-                # Adding to dictionary position, date of birth, height and link
-                empty_player_dictionary["position"] = player_data[0].next
-                empty_player_dictionary["date_of_birth"] = player_data[2].next.strip()
-                empty_player_dictionary["height"] = player_data[3].next
-                empty_player_dictionary["player_link"] = URL
-
-                # # convert and add to Json file
-                with open("players.json", "a") as outfile:
-                    outfile.write(json.dumps(empty_player_dictionary, indent=4))
-
-                id += 1
-
-            else:
-                print("end of Parsing")
-                break
-
-        except IndexError:
-            id+=1
-            continue
 
 
+def pareser(data, url, id):
+    try:
+        soup = BeautifulSoup(data.content, "html.parser")
+        player_data = soup.findAll("div", class_="info")
+        player_country = soup.findAll("span", class_="playerCountry")
+        picture = soup.findAll('img')[42]
+        # Adding to dictionary name, country of origin
+        empty_player_dictionary["id"] = id
+        empty_player_dictionary["name"] = picture.get('alt')
+        empty_player_dictionary["country"] = player_country[0].next
+        if "Photo-Missing" in picture.get('src') and picture.get('data-player'):
+            a_string = str(picture.get('src')).replace("Photo-Missing", str(picture.get('data-player')))
+            # Adding to dictionary image_link
+            empty_player_dictionary["image_link"] = a_string.replace("//", "")
+        else:
+            # Adding to dictionary image_link
+            empty_player_dictionary["image_link"] = picture.get('src').replace("//", "")
+        # Adding to dictionary position, date of birth, height and link
+
+        empty_player_dictionary["position"] = player_data[0].next
+        empty_player_dictionary["date_of_birth"] = player_data[2].next.strip()
+        empty_player_dictionary["height"] = player_data[3].next
+        empty_player_dictionary["player_link"] = url
+        print(f"{id} - {empty_player_dictionary}")
+        add_to_json(empty_player_dictionary)
+    except:
+        pass
+
+
+def add_to_json(data):
+    with open("players.json", "a") as outfile:
+        outfile.write(json.dumps(data, indent=4))
+
+
+def scrape_player_data(loop = True):
+    id = 1
+    while loop:
+        rec = requests.get(url=URL + str(id), headers=headers)
+        if rec.status_code == 200:
+            pareser(rec, URL + str(id), id)
+            id += 1
+        else:
+            print("end of Parsing")
+            loop = False
+            id += 1
 
 
 scrape_player_data()
+
 
 
