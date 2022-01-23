7d5c3827590284b83e5c6e95f01779ba3dc947a6
Alex Petricenco <apetricenco@wyze.com>
2022-01-22 18:21:07 -0800

    add async_data by 50

--- test/main.py
+++ test/main.py
@@ -1,12 +1,16 @@
-import requests
+
 import json
+
+import grequests
 
 from bs4 import BeautifulSoup
 
 empty_player_dictionary = {}
 
+STEP = 50
+
 URL = "https://www.premierleague.com/players/"
-headers = {"accept": "*/*",
+HEDERS = {"accept": "*/*",
                "accept-language": "en-US,en;q=0.9",
                "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
                "origin": "https://www.premierleague.com",
@@ -20,32 +24,39 @@ headers = {"accept": "*/*",
                "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36}"}
 
 
+def get_async_data(urls):
+    reqs = [grequests.get(link, headers=HEDERS) for link in urls]
+    responses = grequests.map(reqs)
+    return responses
 
-def pareser(data, url, id):
+
+def pareser(data):
     try:
         soup = BeautifulSoup(data.content, "html.parser")
         player_data = soup.findAll("div", class_="info")
         player_country = soup.findAll("span", class_="playerCountry")
         picture = soup.findAll('img')[42]
+
         # Adding to dictionary name, country of origin
-        empty_player_dictionary["id"] = id
         empty_player_dictionary["name"] = picture.get('alt')
         empty_player_dictionary["country"] = player_country[0].next
         if "Photo-Missing" in picture.get('src') and picture.get('data-player'):
             a_string = str(picture.get('src')).replace("Photo-Missing", str(picture.get('data-player')))
+
             # Adding to dictionary image_link
             empty_player_dictionary["image_link"] = a_string.replace("//", "")
         else:
             # Adding to dictionary image_link
             empty_player_dictionary["image_link"] = picture.get('src').replace("//", "")
-        # Adding to dictionary position, date of birth, height and link
 
+        # Adding to dictionary position, date of birth, height and link
         empty_player_dictionary["position"] = player_data[0].next
         empty_player_dictionary["date_of_birth"] = player_data[2].next.strip()
         empty_player_dictionary["height"] = player_data[3].next
-        empty_player_dictionary["player_link"] = url
-        print(f"{id} - {empty_player_dictionary}")
+        empty_player_dictionary["player_link"] = data.url
+        print(f"{empty_player_dictionary}")
         add_to_json(empty_player_dictionary)
+
     except:
         pass
 
@@ -55,20 +66,14 @@ def add_to_json(data):
         outfile.write(json.dumps(data, indent=4))
 
 
-def scrape_player_data(loop = True):
-    id = 1
-    while loop:
-        rec = requests.get(url=URL + str(id), headers=headers)
-        if rec.status_code == 200:
-            pareser(rec, URL + str(id), id)
-            id += 1
-        else:
-            print("end of Parsing")
-            loop = False
-            id += 1
+def scrape_player_data():
+    list_a = [f"{URL}{count}" for count in range(1, 2000)]
+    output = [list_a[i:i + STEP] for i in range(0, len(list_a), STEP)]
+    for url in output:
+        for resopnd in get_async_data(url):
+            if resopnd and resopnd.status_code == 200:
+                pareser(resopnd)
+
 
 
 scrape_player_data()
-
-
-
