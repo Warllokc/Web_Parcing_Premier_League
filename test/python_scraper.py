22772cc012c13d654344006534de0cb735b38836
Alex Petricenco <apetricenco@wyze.com>
2022-02-02 13:19:01 -0800

    create add adn delete functions

--- test/python_scraper.py
+++ test/python_scraper.py
@@ -1,3 +1,12 @@
-from sqlalchemy import create_engine
-
-engine = create_engine
+def save_trading_report(trading_report: list, news_data):
+    data_to_save = []
+    for report in trading_report:
+        data_to_save.append(report.text)
+    report = TradingReport()
+    report.company_title = data_to_save[0].replace("‏", '').replace(" ",'').strip()
+    report.company_name = data_to_save[1].replace("‏", '').replace(" ",'').strip()
+    report.buy_volume = data_to_save[2].replace("‏", '').replace(" ",'').replace(",",'').replace(" ",'')
+    report.sell_volume = data_to_save[3].replace("‏", '').replace(" ",'').replace(",",'').replace(" ",'')
+    report.report_data = datetime.datetime.strptime(f"{news_data}T10:10:10-0005", '%Y-%m-%dT%H:%M:%S%z')
+    session.add(report)
+    session.commit()
---
+++ test/sql_alchemy_table.py
@@ -0,0 +1,68 @@
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import sessionmaker
+from  sqlalchemy.ext.declarative import declarative_base
+from tqdm import tqdm
+engine = create_engine('postgresql://oisolqzxghhhjp:e3a898afdb19bea8c2b6864347f1da303f6fdfe443637715112252cffd87d9d6@'
+                       'ec2-3-224-157-224.compute-1.amazonaws.com:5432/d8uo0fbra6958c', echo=True)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+id = 1
+
+Base = declarative_base()
+
+class Player_Table(Base):
+    __tablename__ = "Primier_League_players"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50))
+    country = Column(String(50))
+    image_link = Column(String(150), unique=True)
+    position = Column(String(30))
+    date_of_birth = Column(String(15))
+    height = Column(String(10))
+    player_link = Column(String(100), unique=True)
+
+Base.metadata.create_all(engine)
+
+def add_player_data(name, country, image, position, date, height,link):
+    global id
+    players_table = session.query(Player_Table).all()
+    pbar = tqdm(total=len(players_table))
+    player_data = Player_Table()
+
+    for player in players_table:
+        key = session.query(Player_Table).filter_by(id=player.id).first()
+        if key in players_table:
+                id+=1
+        else:
+            continue
+    player_data.id = id
+    player_data.name = name
+    player_data.country = country
+    player_data.image_link = image
+    player_data.position = position
+    player_data.date_of_birth = date
+    player_data.height = height
+    player_data.player_link = link
+
+    pbar.update(1)
+    id+=1
+    session.add(player_data)
+    session.commit()
+
+def delete_player_data():
+    players_data = session.query(Player_Table).all()
+    pbar = tqdm(total=len(players_data))
+    for player in players_data:
+        premier = session.query(Player_Table).filter_by(name=player.name).first()
+        premier.status = True
+        pbar.update(1)
+    session.delete(premier)
+    session.commit()
+
+
+############# parsing JSON file
+# add_player_data("David Seaman","England","resources.premierleague.com/premierleague/photos/players/250x250/p9.png",
+#                 "Goalkeeper", "19/09/1963", "193cm", "https://www.premierleague.com/players/1")
+delete_player_data()
